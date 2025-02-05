a1aad11be942fb673ab93f3154caff1313803365
JoaquinDM06 <jj.dominguezmejia@ugto.mx>
2025-02-05 10:39:04 -0600

    Modificacion del 05-02-25

--- Jan29/programa_hola.c
+++ Jan29/programa_hola.c
@@ -5,4 +5,6 @@ lineas de escritura.
 En programacion no importan
 si usas o no los acentos en la escritura.
 Creado el 29 de enero, 2025
+Modificacion de la clase del 
+dia 05 de febrero de 2025
 */
