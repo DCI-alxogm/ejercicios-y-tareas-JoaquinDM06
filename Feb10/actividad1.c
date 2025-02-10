b2b606f4a9200d6bb814041b4903c40941987f4a
JoaquinDM06 <jj.dominguezmejia@ugto.mx>
2025-02-10 11:45:27 -0600

    Mi segundo programa del 10/02/25

---
+++ Feb10/actividad1.c
@@ -0,0 +1,11 @@
+/* Creado el 10 de Febrero de 2025 por Joaquin Dominguez*/
+#include<stdio.h>
+
+int main()
+{
+
+	printf("Hola Joaquin \n");
+	printf("Esta es mi primera actividad \n");
+}
+
+/*Fin de la actividad*/
