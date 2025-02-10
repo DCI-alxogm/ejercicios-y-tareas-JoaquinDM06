a8a4ed504b2d187655fdccef00a5b67416b9ec99
JoaquinDM06 <jj.dominguezmejia@ugto.mx>
2025-02-10 11:20:07 -0600

    Hoy cree mi primer programa

---
+++ Feb10/primer_programa_j.c
@@ -0,0 +1,10 @@
+/*Creado el 10 de Febrero de 2025 por Joaquin Dominguez*/
+#include<stdio.h>
+
+int main()
+{
+
+	printf("Hola a todos \n");
+	printf("Este es mi primer programa\n");
+}
+/*Ya termine de escribir*/
