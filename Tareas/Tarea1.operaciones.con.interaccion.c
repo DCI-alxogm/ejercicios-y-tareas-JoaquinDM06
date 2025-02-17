8c6570c4a8366ab5523ee50417722e8d87ff747a
JoaquinDM06 <jj.dominguezmejia@ugto.mx>
2025-02-17 12:00:32 -0600

    Tarea 1

---
+++ Tareas/Tarea1.operaciones.con.interaccion.c
@@ -0,0 +1,14 @@
+/* 
+Febrero 17, 2025
+Joaquin Dominguez
+Primera tarea en donde se tienen que ejecutar operaciones pero nosotros tenemos que ingresar los numeros desde la terminal.
+*/
+
+
+#include <stdlib.h>
+#include <stdlib.h>
+
+int main(){
+	float a,b,c,d;
+	
+}
