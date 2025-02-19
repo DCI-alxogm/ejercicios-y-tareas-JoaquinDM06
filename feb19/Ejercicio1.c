5fc10ece1d97fd16bd87d30b3f11553b294af5c4
JoaquinDM06 <jj.dominguezmejia@ugto.mx>
2025-02-19 11:55:38 -0600

    Avance del ejercicio del 19feb

---
+++ feb19/Ejercicio1.c
@@ -0,0 +1,19 @@
+/*
+Convercion de cordenadas cilindricas y esfericas a partir de X,Y,Z.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	float x, y, z: //variables iniciales
+	float re, tan_teta, fi, rc, teta; //variables que tenemos que calcular
+	//Cuerpo del programa
+	printf(" coordenada en x \n");
+	scanf("%f", &x)
+	
+	
+	
+
+}
