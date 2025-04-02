802f1e9ed152843a78d01ce26d9980b635d6e7dd
root <root@Joaquin.>
2025-04-01 19:31:59 -0600

    Punto numero 3 de los ejercicios de 31 de marzo con do... while

--- mar31/positivo.c
+++ mar31/positivo.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 int main() {
+    int num;
+    char opcion;
 
-int num;
-	 do {
-	printf("ingrese un numero positivo\n");
-	scanf("%d", &num);
+    do {
+        do {
+            printf("Ingrese un número positivo:\n");
+            scanf("%d", &num);
+        } while (num <= 0);
 
-	} while (num <= 0);
-	printf("numero ingresado; %d\n", num);
+        printf("El número ingresado: %d es positivo\n", num);
 
-return 0;
+        printf("¿Desea ingresar otro número? (s/n): ");
+        scanf(" %c", &opcion); // Espacio antes de %c para evitar problemas con el buffer
+
+    } while (opcion == 's' || opcion == 'S');
+
+    printf("Programa finalizado.\n");
+
+    return 0;
 }
 
--- mar31/x_cuadrada.c
+++ mar31/x_cuadrada.c
@@ -2,6 +2,9 @@
 
 int main() {
     int x, x_final, incremento;
+    char opcion;
+
+  do{
 
     printf("Ingrese el valor inicial: ");
     scanf("%d", &x);
@@ -10,10 +13,18 @@ int main() {
     printf("Ingrese el incremento: ");
     scanf("%d", &incremento);
 
+
     do {
         printf("x = %d, x^2 = %d\n", x, x * x);
         x += incremento;
     } while (x <= x_final);
+
+        printf("Desea realizar la operacion con otros valores? (s/n): ");
+        scanf(" %c", &opcion);
+
+  } while (opcion == 's' || opcion == 'S');
+
+    printf("Programa finalizado.\n");
 
     return 0;
 }
